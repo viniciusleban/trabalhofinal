f7fb8d68456dae07dc761b568eecd429cc06192e
Vinicius Leban <viniciusl@grupotozzo.local>
2026-06-15 17:33:13 -0300

    Atualizado o script da criação dos usuarios

--- banco/03_seed.sql
+++ banco/03_seed.sql
@@ -1,7 +1,11 @@
 INSERT INTO usuario (nome, email, senha_hash, papel) VALUES
-('Vinicius Leban', 'vinicius@g8.com', 'SUBSTITUA_PELO_HASH_DE_gerar-hash.js', 'farmaceutico'),
-('Pedro Tormem', 'pedro@g8.com', 'SUBSTITUA_PELO_HASH_DE_gerar-hash.js', 'farmaceutico'),
-('Gabriel Sartori', 'gabriel@g8.com', 'SUBSTITUA_PELO_HASH_DE_gerar-hash.js', 'auditor');
+('Vinicius Leban', 'vinicius@g8.com', '$2a$10$vwhHU/8rV15jvHsWWgCpOOzxeb81pr23CBt0IBaUpfGiMCRwFFhSG', 'farmaceutico'),
+('Pedro Tormem', 'pedro@g8.com', '$2a$10$vwhHU/8rV15jvHsWWgCpOOzxeb81pr23CBt0IBaUpfGiMCRwFFhSG', 'farmaceutico'),
+('Gabriel Sartori', 'gabriel@g8.com', '$2a$10$vwhHU/8rV15jvHsWWgCpOOzxeb81pr23CBt0IBaUpfGiMCRwFFhSG', 'auditor');
+
+-- senhas 'senha123' geradas pelo gerar-hash.js
+-- cd backend
+-- node .\gerar-hash.js
 
 INSERT INTO medicamento (nome_comercial, principio_ativo, forma_farmaceutica, concentracao, unidade_medida, preco_unitario, estoque_atual) VALUES
 ('Dipirona Sodica', 'Dipirona', 'Comprimido', '500mg', 'comprimido', 0.45, 500),
